43435fd034252eda5d6a7b4dc5d9594d2cdb505c
Yakov Rogov <rogov@haulmont.com>
2020-02-26 17:10:31 +0400

    refactored check functions

--- drag-n-drop.js
+++ drag-n-drop.js
@@ -48,8 +48,7 @@ function makeDraggable(svg) {
       evt.preventDefault();
       var coord = getMousePosition(evt);
 
-      var canTranslate = isEmpty(evt.target, coord);
-      console.log(canTranslate);
+      var canTranslate = isEmpty(evt);
       if(canTranslate) {
         transform.setTranslate(coord.x - offset.x, coord.y - offset.y);
       }
@@ -61,15 +60,95 @@ function makeDraggable(svg) {
   }
 }
 
-function isEmpty(elem, coords) {
+function isEmpty(event) {
+  var elem = event.target;
+  var boundingRect = elem.getBoundingClientRect();
+
   elem.style.display = 'none';
 
-  var belowElement = document.elementFromPoint(coords.x, coords.y);
-  var isEmpty = isParent(elem, belowElement);
+  var coords = getCoords(event, boundingRect);
+  var belowElement = null;
+
+  var isEmpty = coords.reduce(function (result, currentValue) {
+    belowElement = document.elementFromPoint(
+      currentValue.x,
+      currentValue.y
+    );
+
+    return isParent(elem, belowElement) && result;
+  }, true);
+
 
   elem.style.display = 'block';
 
   return isEmpty;
+}
+
+function getCoords(event, rect) {
+  var xStep = Math.floor(rect.width / 10);
+  var yStep = Math.floor(rect.height / 10);
+
+  var coords = [];
+  var edgeCoords = [];
+
+  if(event.movementX > 0) {
+    edgeCoords = Array.from({length: yStep}, function(v, i) {
+      return {
+        x: rect.right,
+        y: rect.top + i * yStep
+      }
+    }).concat([{
+      x: rect.right,
+      y: rect.bottom
+    }]);
+
+    coords = coords.concat(edgeCoords);
+    console.log(coords);
+  }
+
+  if(event.movementX < 0) {
+    edgeCoords = Array.from({length: yStep}, function(v, i) {
+      return {
+        x: rect.left,
+        y: rect.top + i * yStep
+      }
+    }).concat([{
+      x: rect.left,
+      y: rect.bottom
+    }]);
+
+    coords = coords.concat(edgeCoords);
+  }
+
+  if(event.movementY > 0) {
+    edgeCoords = Array.from({length: xStep}, function(v, i) {
+      return {
+        x: rect.left + i * xStep,
+        y: rect.bottom
+      }
+    }).concat([{
+      x: rect.right,
+      y: rect.bottom
+    }]);
+
+    coords = coords.concat(edgeCoords);
+  }
+
+  if(event.movementY < 0) {
+    edgeCoords = Array.from({length: xStep}, function(v, i) {
+      return {
+        x: rect.left + i * xStep,
+        y: rect.top
+      }
+    }).concat([{
+      x: rect.right,
+      y: rect.top
+    }]);
+
+    coords = coords.concat(edgeCoords);
+  }
+
+  return coords;
 }
 
 function isParent(element, elementToCheck) {
@@ -90,6 +169,7 @@ function isParent(element, elementToCheck) {
 function isSvg(element) {
   return element instanceof SVGSVGElement;
 }
+
 
 var svg = document.querySelectorAll('svg');
 svg.forEach(function (element) {
